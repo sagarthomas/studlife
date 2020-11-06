df6774e0d7812f7ccc81bbcc3f991faba2d0c40d
Matthew-Dombrady <matthew.dombrady@gmail.com>
2020-11-05 20:40:17 -0500

    Added editing of budgets

--- src/components/BudgetPage.js
+++ src/components/BudgetPage.js
@@ -1,11 +1,16 @@
 import React from "react";
-import { Space, Card, Divider, Table, Progress, Typography, Popover, Button } from 'antd';
+import { Space, Card, Divider, Table, Progress, Typography, Popover, Button, Input, Select } from 'antd';
 
 class BudgetPage extends React.Component {
   constructor(props) {
     super(props);
 
-    this.state = {budgets: [{category: "Food", budget: "200", spent: "50"}, {category: "School", budget: "1000", spent: "750"}]};
+    var budgets = [{category: "Food", budget: "200", spent: "50"}, {category: "School", budget: "1000", spent: "750"}];
+
+    this.state = {budgets: budgets, currentEditCategory: budgets[0].category, currentEditBudget: budgets[0].budget, input: ""};
+
+    this.updateBudget = this.updateBudget.bind(this);
+    
   }
 
   findPercent() {
@@ -17,7 +22,41 @@ class BudgetPage extends React.Component {
       spent += parseFloat(element.spent);
     });
 
-    return ((spent/budget)*100).toFixed(1);
+    return ((spent/budget)*100).toFixed(0);
+
+  }
+
+  findBudgetToEdit(value) {
+
+    var currentEditBudget = "";
+
+    this.state.budgets.forEach(element => {
+      if (element.category == value) {
+        currentEditBudget = element.budget;
+      }
+    });
+
+    this.setState({ 
+      currentEditCategory: value,
+      currentEditBudget: currentEditBudget
+    })
+  }
+
+  updateBudget() {
+    
+    var budgets = this.state.budgets;
+
+    this.state.budgets.forEach(element => {
+      if (element.category == this.state.currentEditCategory) {
+        element.budget = this.state.input;
+      }
+    });
+
+    this.setState({ 
+      budgets: budgets
+    })
+
+    console.log(this.state.budgets);
 
   }
 
@@ -44,6 +83,13 @@ class BudgetPage extends React.Component {
       },
     ];
 
+    const editOptions = [];
+
+    this.state.budgets.forEach(element => {
+      editOptions.push({value: element.category, label: element.category});
+    });
+
+    const onChange = value => this.findBudgetToEdit(value);
 
     return (
       <Space direction="vertical">
@@ -52,6 +98,15 @@ class BudgetPage extends React.Component {
         <Divider />
         <Title level={2}>A breakdown of your budget</Title>
         <Table columns={columns} dataSource={this.state.budgets} />
+        <Divider />
+        <Title level={5}>Edit your budgets</Title>
+        <Input.Group compact>
+          <Select defaultValue={this.state.budgets[0].category} options={editOptions} onChange={onChange} >
+          </Select>
+          <Input style={{ width: '50%' }} onChange={(event) => this.setState({input: event.target.value})} suffix={
+            <Button type="primary" onClick={this.updateBudget} >OK</Button>
+          } />
+        </Input.Group>
       </Space>
     );  
   }
