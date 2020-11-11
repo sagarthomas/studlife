ccc283276f8c75ecbc2477b714e5ff05c3dd6b5d
Ardhendu Barge <bargea@mcmaster.ca>
2020-11-10 22:13:17 -0500

    added edit button and delete functionality with temporary button

--- src/components/ExpensePage.js
+++ src/components/ExpensePage.js
@@ -1,43 +1,103 @@
 import React from "react";
-import { Table, Tag, Space } from 'antd';
+import { Table, Tag, Space, Input, Button, Popconfirm, Form } from 'antd';
 import { Resizable } from 'react-resizable';
 import moment from 'moment';
+import "../App.css";
 
-const ResizableTitle = props => {
-  const { onResize, width, ...restProps } = props;
+const EditableContext = React.createContext();
 
-  if (!width) {
-    return <th {...restProps} />;
-  }
+const EditableRow = ({ form, index, ...props }) => (
+  <EditableContext.Provider value={form}>
+    <tr {...props} />
+  </EditableContext.Provider>
+);
 
-  return (
-    <Resizable
-      width={width}
-      height={0}
-      handle={
-        <span
-          className="react-resizable-handle"
-          onClick={e => {
-            e.stopPropagation();
-          }}
-        />
+const EditableFormRow = Form.create(EditableRow);
+
+class EditableCell extends React.Component {
+  state = {
+    editing: false,
+  };
+
+  toggleEdit = () => {
+    const editing = !this.state.editing;
+    this.setState({ editing }, () => {
+      if (editing) {
+        this.input.focus();
       }
-      onResize={onResize}
-      draggableOpts={{ enableUserSelectHack: false }}
-    >
-      <th {...restProps} />
-    </Resizable>
-  );
-};
+    });
+  };
+
+  save = e => {
+    const { record, handleSave } = this.props;
+    this.form.validateFields((error, values) => {
+      if (error && error[e.currentTarget.id]) {
+        return;
+      }
+      this.toggleEdit();
+      handleSave({ ...record, ...values });
+    });
+  };
+
+  renderCell = form => {
+    this.form = form;
+    const { children, dataIndex, record, title } = this.props;
+    const { editing } = this.state;
+    return editing ? (
+      <Form.Item style={{ margin: 0 }}>
+        {form.getFieldDecorator(dataIndex, {
+          rules: [
+            {
+              required: true,
+              message: `${title} is required.`,
+            },
+          ],
+          initialValue: record[dataIndex],
+        })(<Input ref={node => (this.input = node)} onPressEnter={this.save} onBlur={this.save} />)}
+      </Form.Item>
+    ) : (
+      <div
+        className="editable-cell-value-wrap"
+        style={{ paddingRight: 24 }}
+        onClick={this.toggleEdit}
+      >
+        {children}
+      </div>
+    );
+  };
+
+  render() {
+    const {
+      editable,
+      dataIndex,
+      title,
+      record,
+      index,
+      handleSave,
+      children,
+      ...restProps
+    } = this.props;
+    return (
+      <td {...restProps}>
+        {editable ? (
+          <EditableContext.Consumer>{this.renderCell}</EditableContext.Consumer>
+        ) : (
+          children
+        )}
+      </td>
+    );
+  }
+}
 
 class ExpensePage extends React.Component {
-  state = {
-    columns: [
+  constructor(props) {
+    super(props);
+    this.columns = [
       {
         title: 'No.',
         dataIndex: 'number',
         key: 'number',
-        width: 50,
+        width: 30,
       },
       {
         title: 'Type',
@@ -71,88 +131,217 @@ class ExpensePage extends React.Component {
         dataIndex: 'frequency',
         key: 'frequency',
         width: 200,
+        sorter: (a, b) => a.frequency.length - b.frequency.length
       },
       {
-        title: 'Action',
-        key: 'action',
-        render: () => <a>Delete</a>,
+        title: 'operation',
+        dataIndex: 'operation',
+        render: (text, record) =>
+          this.state.dataSource.length >= 1 ? (
+            <div>
+              <Popconfirm title="Are you sure to delete this expense" onConfirm={() => this.handleDelete(record.key)}>
+                <a style={{marginRight: 5}}>Delete</a>
+              </Popconfirm>
+              <Popconfirm title="Are you sure to edit this expense?" >
+                <a>Edit</a>
+              </Popconfirm>
+            </div>
+          ) : null,
       },
-    ],
-    data: [
-      {
-        key: '1',
-        number: 1,
-        type: 'Food',
-        expense: 'Grocery',
-        amount: 50,
-        ddate: moment('10-15-2020').format('L'),
-        frequency: 'Bi-weekly'
-      },
-      {
-        key: '2',
-        number: 2,
-        type: 'Education',
-        expense: 'Textbooks',
-        amount: 300,
-        ddate: moment('10-28-2020').format('L'),
-        frequency: 'Once',
-      },
-      {
-        key: '3',
-        number: 3,
-        type: 'Miscellanous',
-        expense: 'Internet',
-        amount: 65,
-        ddate: moment('10-25-2020').format('L'),
-        frequency: 'Monthly',
-      },
-    ]    
+    ];
+
+    this.state = {
+      dataSource: [
+        {
+          key: '1',
+          number: 1,
+          type: 'Food',
+          expense: 'Grocery',
+          amount: 50,
+          ddate: moment('10-15-2020').format('L'),
+          frequency: 'Bi-weekly'
+        },
+        {
+          key: '2',
+          number: 2,
+          type: 'Education',
+          expense: 'Textbooks',
+          amount: 300,
+          ddate: moment('10-28-2020').format('L'),
+          frequency: 'Once',
+        },
+        {
+          key: '3',
+          number: 3,
+          type: 'Miscellanous',
+          expense: 'Internet',
+          amount: 65,
+          ddate: moment('10-25-2020').format('L'),
+          frequency: 'Monthly',
+        },
+      ],
+      count: 3,
+    };
+  }
+
+  handleDelete = key => {
+    const dataSource = [...this.state.dataSource];
+    this.setState({ dataSource: dataSource.filter(item => item.key !== key) });
   };
 
-  components = {
-    header: {
-      cell: ResizableTitle,
-    },
-  };
-
-  handleResize = index => (e, { size }) => {
-    this.setState(({ columns }) => {
-      const nextColumns = [...columns];
-      nextColumns[index] = {
-        ...nextColumns[index],
-        width: size.width,
-      };
-      return { columns: nextColumns };
+  handleAdd = () => {
+    const { count, dataSource } = this.state;
+    const newData = {
+      key: count+1,
+      number: count+1,
+      type: `Miscellanous`,
+      expense: `Cell-phone bill`,
+      amount: 40,
+      ddate: moment('10-22-2020').format('L'),
+      frequency: 'Monthly'
+    };
+    this.setState({
+      dataSource: [...dataSource, newData],
+      count: count + 1,
     });
   };
 
-  render() {
-    const columns = this.state.columns.map((col, index) => ({
-      ...col,
-      onHeaderCell: column => ({
-        width: column.width,
-        onResize: this.handleResize(index),
-      }),
-    }));
+  handleSave = row => {
+    const newData = [...this.state.dataSource];
+    const index = newData.findIndex(item => row.key === item.key);
+    const item = newData[index];
+    newData.splice(index, 1, {
+      ...item,
+      ...row,
+    });
+    this.setState({ dataSource: newData });
+  };
 
-    return <Table bordered components={this.components} columns={columns} dataSource={this.state.data} />;
+  render() {
+    const { dataSource } = this.state;
+    const components = {
+      body: {
+        row: EditableFormRow,
+        cell: EditableCell,
+      },
+    };
+    const columns = this.columns.map(col => {
+      if (!col.editable) {
+        return col;
+      }
+      return {
+        ...col,
+        onCell: record => ({
+          record,
+          editable: col.editable,
+          dataIndex: col.dataIndex,
+          title: col.title,
+          handleSave: this.handleSave,
+        }),
+      };
+    });
+    return (
+      <div>
+        <Button onClick={this.handleAdd} type="primary" style={{ marginBottom: 16 }}>
+          Add a row
+        </Button>
+        <Table
+          components={components}
+          rowClassName={() => 'editable-row'}
+          bordered
+          dataSource={dataSource}
+          columns={columns}
+        />
+      </div>
+    );
   }
 }
 
-// class ExpensePage extends React.Component {
-//   constructor(props) {
-//     super(props);
+// new
+
+// const ResizableTitle = props => {
+//   const { onResize, width, ...restProps } = props;
+
+//   if (!width) {
+//     return <th {...restProps} />;
 //   }
 
-//   ExpTable = () => {
-//     const dataSource = [
+//   return (
+//     <Resizable
+//       width={width}
+//       height={0}
+//       handle={
+//         <span
+//           className="react-resizable-handle"
+//           onClick={e => {
+//             e.stopPropagation();
+//           }}
+//         />
+//       }
+//       onResize={onResize}
+//       draggableOpts={{ enableUserSelectHack: false }}
+//     >
+//       <th {...restProps} />
+//     </Resizable>
+//   );
+// };
+
+// class ExpensePage extends React.Component {
+//   state = {
+//     columns: [
+      // {
+      //   title: 'No.',
+      //   dataIndex: 'number',
+      //   key: 'number',
+      //   width: 50,
+      // },
+      // {
+      //   title: 'Type',
+      //   dataIndex: 'type',
+      //   key: 'type',
+      //   width: 200,
+      //   sorter: (a, b) => a.type.length - b.type.length,
+      // },
+      // {
+      //   title: 'Expense',
+      //   dataIndex: 'expense',
+      //   key: 'expense',
+      //   width: 200,
+      //   sorter: (a, b) => a.expense.length - b.expense.length,
+      // },
+      // {
+      //   title: 'Amount',
+      //   dataIndex: 'amount',
+      //   width: 200,
+      //   sorter: (a, b) => a.amount - b.amount,
+      // },
+      // {
+      //   title: 'Due date',
+      //   dataIndex: 'ddate',
+      //   key: "ddate",
+      //   width: 200,
+      //   sorter: (a, b) => new Date(a.ddate) - new Date(b.ddate)
+      // },
+      // {
+      //   title: 'Frequency',
+      //   dataIndex: 'frequency',
+      //   key: 'frequency',
+      //   width: 200,
+      // },
+//       {
+//         title: 'Action',
+//         key: 'action',
+//         render: () => <a>Delete</a>,
+//       },
+//     ],
+//     data: [
       // {
       //   key: '1',
       //   number: 1,
       //   type: 'Food',
       //   expense: 'Grocery',
-      //   amount: '$50',
-      //   ddate: "Oct 15",
+      //   amount: 50,
+      //   ddate: moment('10-15-2020').format('L'),
       //   frequency: 'Bi-weekly'
       // },
       // {
@@ -160,64 +349,49 @@ class ExpensePage extends React.Component {
       //   number: 2,
       //   type: 'Education',
       //   expense: 'Textbooks',
-      //   amount: '$300',
-      //   ddate: "Oct 28",
+      //   amount: 300,
+      //   ddate: moment('10-28-2020').format('L'),
       //   frequency: 'Once',
       // },
-//     ];
-    
-//     const columns = [
       // {
-      //   title: 'No.',
-      //   dataIndex: 'number',
-      //   key: 'number',
+      //   key: '3',
+      //   number: 3,
+      //   type: 'Miscellanous',
+      //   expense: 'Internet',
+      //   amount: 65,
+      //   ddate: moment('10-25-2020').format('L'),
+      //   frequency: 'Monthly',
       // },
-      // {
-      //   title: 'Type',
-      //   dataIndex: 'type',
-      //   key: 'type',
-      // },
-      // {
-      //   title: 'Expense',
-      //   dataIndex: 'expense',
-      //   key: 'expense',
-      // },
-//       {
-//         title: 'Amount',
-//         dataIndex: 'amount',
-//         key: 'amount',
-//         sorter: {
-//           compare: (a, b) => a.amount - b.amount,
-//         },
-//       },
-//       {
-//         title: 'Due Date',
-//         dataIndex: 'ddate',
-//         key: 'ddate',
-//       },
-      // {
-      //   title: 'Frequency',
-      //   dataIndex: 'frequency',
-      //   key: 'frequency',
-      // },
-//     ];
+//     ]    
+//   };
 
-//     function onChange(pagination, filters, sorter, extra) {
-//       console.log('params', pagination, filters, sorter, extra);
-//     }
+//   components = {
+//     header: {
+//       cell: ResizableTitle,
+//     },
+//   };
 
-//     return(<Table dataSource={dataSource} columns={columns} onChange={onChange}/>)
-//   }
-
-  
+//   handleResize = index => (e, { size }) => {
+//     this.setState(({ columns }) => {
+//       const nextColumns = [...columns];
+//       nextColumns[index] = {
+//         ...nextColumns[index],
+//         width: size.width,
+//       };
+//       return { columns: nextColumns };
+//     });
+//   };
 
 //   render() {
-//     return (
-//       <div>
-//         <h1><b>EXPENSES</b></h1>
-//         <this.ExpTable/>
-//       </div>
-//     );
+//     const columns = this.state.columns.map((col, index) => ({
+//       ...col,
+//       onHeaderCell: column => ({
+//         width: column.width,
+//         onResize: this.handleResize(index),
+//       }),
+//     }));
+
+//     return <Table bordered components={this.components} columns={columns} dataSource={this.state.data} />;
 //   }
 // }
 
